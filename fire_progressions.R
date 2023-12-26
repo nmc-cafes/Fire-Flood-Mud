3daa7d086013c6b4ebd623d751cd49143aeb2346
ntutland <56928897+ntutland@users.noreply.github.com>
2023-12-26 14:45:40 -0800

    Create fire_progressions.R

---
+++ fire_progressions.R
@@ -0,0 +1,34 @@
+library(here)
+library(terra)
+library(tidyverse)
+library(tidyterra)
+
+fire_names <- c("CubCreek2")
+caps_names <- c("CUB_CREEK_2")
+dfs <- list()
+
+for(i in 1:length(fire_names)){
+  prog <- vect(here(fire_names[i],
+                    "Fire_Progression", 
+                    paste0(caps_names[i], "_2021_PROGRESSION_CORRECTED.shp")))
+  
+  sites <- vect(here(fire_names[i],
+                     paste0(fire_names[i],"_sample_sites_NJT.shp")))
+  
+  prog <- project(prog,sites)
+  df <- data.frame("Fire_Name" = rep(NA,nrow(sites)),
+                   "Site_Name" = rep(NA,nrow(sites)),
+                   "Fire_Date" = rep(NA,nrow(sites)))
+  
+  for(j in 1:nrow(sites)){
+    all_dates <- terra::extract(prog,sites[j,])
+    all_dates$DateCurren <- as.Date(all_dates$DateCurren)
+    burn_day <- all_dates[all_dates$DateCurren == min(all_dates$DateCurren),]$DateCurren
+    df$Fire_Name[j] <- fire_names[i]
+    df$Site_Name[j] <- sites[j,]$Site_Name
+    df$Fire_Date[j] <- as.character(burn_day)
+  }
+  dfs[[i]] <- df
+}
+
+
