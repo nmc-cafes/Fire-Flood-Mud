37c4bbfaec5255be8bcfa2b875b69aca39f18d5c
ntutland <56928897+ntutland@users.noreply.github.com>
2023-12-27 13:58:16 -0800

    Move fire_progressions code to sample_sites_csv

--- sample_sites_csv.R
+++ sample_sites_csv.R
@@ -1,8 +1,9 @@
 library(here)
 library(tidyverse)
+library(terra)
 library(sf)
 
-fires <- c("Caldor","Dixie","KNP","CedarCreek","Muckamuck")
+fires <- c("Caldor","Dixie","KNP","CedarCreek","CubCreek2")
 states <- data.frame("state"=c(rep("CA",3),rep("WA",2)),
                      "fire"=fires)
 fire_list <- list()
@@ -22,6 +23,36 @@ sample_sites_df <- sample_sites %>%
   mutate(X = st_coordinates(.)[,1],
          Y = st_coordinates(.)[,2]) %>%
   st_drop_geometry() %>%
-  select(Fire_State,Fire_Name,Site_Name,Fire_Date,X,Y)
+  select(Fire_State,Fire_Name,Site_Name,X,Y)
 
-write.csv(sample_sites_df, here("Sample_Sites_NJT.csv"), row.names=F)
+caps_names <- c("CALDOR","DIXIE","KNP_COMPLEX","CEDAR_CREEK","CUB_CREEK_2")
+dfs <- list()
+
+for(i in 1:length(fires)){
+  prog <- vect(here(fires[i],
+                    "Fire_Progression", 
+                    paste0(caps_names[i], "_2021_PROGRESSION_CORRECTED.shp")))
+  
+  sites <- vect(here(fires[i],
+                     paste0(fires[i],"_sample_sites_NJT.shp")))
+  
+  prog <- project(prog,sites)
+  df <- data.frame("Fire_Name" = rep(NA,nrow(sites)),
+                   "Site_Name" = rep(NA,nrow(sites)),
+                   "Fire_Date" = rep(NA,nrow(sites)))
+  
+  for(j in 1:nrow(sites)){
+    all_dates <- terra::extract(prog,sites[j,])
+    all_dates$DateCurren <- as.Date(all_dates$DateCurren)
+    burn_day <- all_dates[all_dates$DateCurren == min(all_dates$DateCurren),]$DateCurren
+    df$Fire_Name[j] <- fires[i]
+    df$Site_Name[j] <- sites[j,]$Site_Name
+    df$Fire_Date[j] <- as.character(burn_day[1])
+  }
+  dfs[[i]] <- df
+}
+fire_dates <- bind_rows(dfs)
+
+final_df <- left_join(sample_sites_df,fire_dates,by=c("Fire_Name","Site_Name"))
+
+write.csv(final_df, here("Sample_Sites_NJT.csv"), row.names=F)
