fb788c01dec8603fc51f8709f25a7944d2d13bcb
ntutland <56928897+ntutland@users.noreply.github.com>
2024-02-15 10:19:43 -0500

    Add for-loop data constructor

--- qf_outputs_to_severity.R
+++ qf_outputs_to_severity.R
@@ -1,5 +1,11 @@
 "
 Read in quicfire outputs as rasters and match them to MTBS severity
+
+Things to loop through
+  - Fires
+  - Sites
+  - Plot size
+  - QF outputs
 "
 
 library(here)
@@ -7,6 +13,7 @@ library(terra)
 library(tidyverse)
 library(tidyterra)
 
+#### DEFINE FUNCTIONS ####
 ext_buff <- function(rst, buffer = -25){
   new_ext <- ext(rst)
   new_ext[1] <- new_ext[1] - buffer
@@ -16,28 +23,91 @@ ext_buff <- function(rst, buffer = -25){
   return(new_ext)
 }
 
-surf_consump <- read.table(here("QF_runs/KNP/KNP_KaweahMiddle_500m/Arrays/surface_consumption.txt"))
-surf_consump_mat <- as.matrix(surf_consump)
+output_to_rst <- function(output, out_arr, plot_bounds){
+  out_mat <- as.matrix(out_arr)
+  out_rst <- rast(nrow = dim(out_mat)[1],
+                  ncol = dim(out_mat)[2],
+                  crs = crs(plot_bounds),
+                  extent = ext(plot_bounds),
+                  vals = out_mat,
+                  names = output)
+  out_rst <- flip(out_rst, direction="vertical")
+  return(out_rst)
+}
 
-plot_bounds <- vect(here("KNP/Sample_Sites/KaweahMiddle/500m/KaweahMiddle_bounds_500m.shp"))
+pts_to_pol <- function(mtbs_pts){
+  mtbs_buf <- buffer(mtbs_pts, width = sqrt(30^2+30^2), quadsegs = 1)
+  mtbs_xy <- crds(mtbs_pts)
+  mtbs_pol <- spin(mtbs_buf, 45, mtbs_xy[,1], mtbs_xy[,2])
+  return(mtbs_pol)
+}
 
-surf_consump_rst <- rast(nrow = dim(surf_consump_mat)[1],
-                         ncol = dim(surf_consump_mat)[2],
-                         crs = crs(plot_bounds),
-                         extent = ext(plot_bounds),
-                         vals = surf_consump_mat,
-                         names = "surface_consumption")
-surf_consump_rst <- flip(surf_consump_rst, direction="vertical")
-surf_consump_crop <- crop(surf_consump_rst, ext_buff(surf_consump_rst))
-surf_consump_agg <- aggregate(surf_consump_crop, fact = 30/res(surf_consump_crop)[1], fun=mean, na.rm=T)
-surf_consump_pts <- as.points(surf_consump_agg)
+#### ASSEMBLE DATASET ####
 
-mtbs <- rast(here("KNP","KNP_dNBR.tif"))
-mtbs_crop <- crop(mtbs, ext(surf_consump_rst))
+first_fire <- T
+for(fire in fires){
+  mtbs <- rast(here(fire,
+                    paste0(fire,"_dNBR.tif")))
+  first_site <- T
+  for(site in sites){ #placeholder
+    first_size <- T
+    for(size in sizes){
+      plot_bounds <- vect(here(fire,
+                               "Sample_Sites",
+                               site,
+                               paste0(size,"m"),
+                               paste0(site,"_bounds_",size,"m.shp")))
+      mtbs_crop <- crop(mtbs, extplot_bounds)
+      mtbs_pts <- as.points(mtbs_crop)
+      mtbs_pol <- pts_to_pol(mtbs_pts)
+      first_output <- T
+      for(output in outputs){
+        out_arr <- read.table(here("QF_runs",
+                                   fire, 
+                                   paste0(fire,"_",site,"_",size,"m"),
+                                   "Arrays",
+                                   paste0(output,".txt")))
+        out_rst <- output_to_rst(output, out_arr, plot_bounds)
+        out_crop <- crop(out_rst, ext_buff(out_rst))
+        if(first_run){
+          out_vect <- terra::extract(out_crop,
+                                     mtbs_pol,
+                                     fun=mean,
+                                     na.rm=T,
+                                     bind=T)
+        } else{
+          out_vect <- terra::extract(out_crop,
+                                     out_vect,
+                                     fun=mean,
+                                     na.rm=T,
+                                     bind=T)
+        }
+        first_output <- F
+      }
+      out_vect$size = size
+      if(first_size){
+        size_vect <- out_vect
+      } else{
+        size_vect <- bind_rows(size_vect, out_vect)
+      }
+      first_size <- F
+    }
+    size_vect$site = site
+    if(first_site){
+      site_vect <- size_vect
+    } else{
+      site_vect <- bind_rows(site_vect,size_vect)
+    }
+    first_site <- F
+  }
+  site_vect$fire <- fire
+  if(first_fire){
+    fire_vect <- site_vect
+  } else{
+    fire_vect <- bind_rows(fire_vect, site_vect)
+  }
+  first_fire <- F
+}
 
-dat_vect <- terra::extract(mtbs_crop, surf_consump_pts, xy=T, ID=F, bind=T)
-dat_df <- as_tibble(dat_vect)
+all_data <- as_tibble(fire_vect)
 
-ggplot() +
-  geom_point(data = dat_df, aes(surface_consumption, KNP_dNBR)) + 
-  theme_bw()
