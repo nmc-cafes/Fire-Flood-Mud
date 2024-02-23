33bf17f02351744a70e6706a0c715779d588a4e0
ntutland <56928897+ntutland@users.noreply.github.com>
2024-02-23 08:47:48 -0500

    Add binned severity to dataset

--- analyze_data.R
+++ analyze_data.R
@@ -3,54 +3,20 @@ library(here)
 library(GGally)
 library(terra)
 
-dat <- read.csv(here("all_data.csv"))
-length(dat$mass_burnt_pct)
-rst <- rast(nrow = 20, 
-            ncol = 20,
-            xmin = -150,
-            xmax = 150,
-            ymin = -150,
-            ymax = 150,
-            vals = dat$dNBR)
-plot(rst)
+dat <- read.csv(here("all_data.csv")) %>%
+  mutate(severity = case_when(severity==1 ~ "unburned",
+                              severity==2 ~ "low",
+                              severity==3 ~ "moderate",
+                              severity==4 ~ "high"))
 
 # explore!
 
-#surface vs canopy consumption
 dat %>%
-  ggplot() +
-  geom_point(aes(surface_consumption,canopy_consumption)) +
-  theme_bw()
-
-#max power vs max reaction rate
-dat %>%
-  ggplot() +
-  geom_point(aes(max_power, max_reaction_rate)) +
-  theme_bw()
-
-#residence times
-dat %>%
-  ggplot() +
-  geom_point(aes(residence_time_power, residence_time_consumption)) +
-  theme_bw()
-
-#mass burnt vs max_power
-dat %>%
-  ggplot() +
-  geom_point(aes(max_power, mass_burnt_pct)) +
-  theme_bw()
-
-## these all seem really correlated
-ggpairs(dat, columns = names(dat)[2:8])
-
-# compare stuff to dnbr
-dat %>%
-  pivot_longer(cols = 2:8,
-               names_to = "var",
-               values_to = "val") %>%
-  ggplot() +
-  geom_point(aes(dNBR,val)) +
-  geom_smooth(aes(dNBR,val), method = "lm") +
-  facet_wrap(.~var, scales = "free_y") +
-  theme_bw()
+  ggplot() + 
+  geom_bar(aes(x=site,y=mass_burnt_pct),stat="identity") +
+  facet_wrap(.~fire, scales="free") +
+  coord_flip() +
+  labs(y="Mass Burnt (%)")
+  theme_bw() +
+  theme(axis.title.x = element_blank())
 
