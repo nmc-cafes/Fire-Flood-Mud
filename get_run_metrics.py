Correct canopy consumption calculation

get_run_metrics.py
@@ -40,7 +40,9 @@ def get_canopy_consumption(sim: SimulationOutputs, arrpath: Path, plot: bool = T
     dens = sim.get_output("fuels-dens")
     dens_init = dens.to_numpy(timestep=0)
     dens_final = dens.to_numpy(len(dens.times) - 1)
-    canopy_consumption = np.sum(dens_init, axis=1) - np.sum(dens_final, axis=1)
+    canopy_consumption = np.sum(dens_init[:, 1:, :, :], axis=1) - np.sum(
+        dens_final[:, 1:, :, :], axis=1
+    )
     canopy_consumption = canopy_consumption[0, :, :]
     if plot:
         plot_array(canopy_consumption, "canopy fuel consumption")