8dfc978979f001302fe730fc14ef16455c1679ef
ntutland <56928897+ntutland@users.noreply.github.com>
2024-04-19 15:29:44 -0400

    Update for duet runs

--- copy_output_arrays.py
+++ copy_output_arrays.py
@@ -4,16 +4,23 @@ from shutil import copytree
 dst_dir = Path(__file__).parent / "Arrays"
 dst_dir.mkdir(exist_ok=True)
 
-fires = ["Caldor","CedarCreek","CubCreek2","Dixie","KNP"]
+fire_site_dict = {
+    "Caldor": ["American", "Camp1", "Camp2", "Camp3", "Strawberry"],
+    "CedarCreek": ["Cedar1", "Cedar2", "Wolf1", "Wolf2", "Wolf3"],
+    "CubCreek2": ["Chewuch", "Eightmile", "Falls", "LambButte", "Sherwood"],
+    "Dixie": ["Genesee", "Kings", "Rush", "Ward", "Yellow"],
+    "KNP": ["AshMountain", "KaweahMiddle", "KaweahNorth", "Potwisha", "Yucca"],
+}
 
-for fire in fires:
+for fire in fire_site_dict.keys():
     fire_src = Path(__file__).parent / "QF_runs" / fire
     fire_dst = dst_dir / fire
     fire_dst.mkdir(exist_ok=True)
-    sites = [path.name for path in fire_src.iterdir() if path.is_dir()]
-    for site in sites:
-        site_dst = fire_dst / site
+    sites = fire_site_dict.get(fire)
+    runs = [f"{fire}_{site}_duet" for site in sites]
+    for run in runs:
+        site_dst = fire_dst / run
         site_dst.mkdir(exist_ok=True)
-        src = fire_src / site / "Arrays"
+        src = fire_src / run / "Arrays"
         dst = site_dst / "Arrays"
-        copytree(src,dst)
+        copytree(src, dst)
