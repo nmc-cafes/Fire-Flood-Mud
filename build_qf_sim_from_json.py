fc1d1f821d8358c516e4adf2de1e623a983c8a25
ntutland <56928897+ntutland@users.noreply.github.com>
2024-05-06 09:22:39 -0400

    Update for expanded sampling ensemble

--- build_qf_sim_from_json.py
+++ build_qf_sim_from_json.py
@@ -1,20 +1,24 @@
 from pathlib import Path
 from quicfire_tools import SimulationInputs
 
-qf_dir = Path(__file__).parent / "QF_runs"
+qf_dir = Path(__file__).parent / "QF_runs" / "Expanded_Sampling"
 
-fire_site_dict = {
-    "Caldor": ["American", "Camp1", "Camp2", "Camp3", "Strawberry"],
-    "CedarCreek": ["Cedar1", "Cedar2", "Wolf1", "Wolf2", "Wolf3"],
-    "CubCreek2": ["Chewuch", "Eightmile", "Falls", "LambButte", "Sherwood"],
-    "Dixie": ["Genesee", "Kings", "Rush", "Ward", "Yellow"],
-    "KNP": ["AshMountain", "KaweahMiddle", "KaweahNorth", "Potwisha", "Yucca"],
-}
+# fire_site_dict = {
+#     "Caldor": ["American", "Camp1", "Camp2", "Camp3", "Strawberry"],
+#     "CedarCreek": ["Cedar1", "Cedar2", "Wolf1", "Wolf2", "Wolf3"],
+#     "CubCreek2": ["Chewuch", "Eightmile", "Falls", "LambButte", "Sherwood"],
+#     "Dixie": ["Genesee", "Kings", "Rush", "Ward", "Yellow"],
+#     "KNP": ["AshMountain", "KaweahMiddle", "KaweahNorth", "Potwisha", "Yucca"],
+# }
 
-for fire in fire_site_dict.keys():
-    for site in fire_site_dict[fire]:
+fires = ["Caldor", "CedarCreek", "CubCreek2", "Dixie", "KNP"]
+ensemble_dir = Path(__file__).parent / "QF_runs" / "Expanded_Sampling"
+
+for fire in fires:
+    sites = [f"{fire}{i}" for i in range(1, 16)]
+    for site in sites:
         print(f"{fire} - {site}")
         run_path = qf_dir / fire / f"{fire}_{site}_duet"
-        json_path = run_path / f"f{site}.json"
+        json_path = run_path / f"{site}.json"
         sim = SimulationInputs.from_json(json_path)
         sim.write_inputs(run_path)
