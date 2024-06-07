0fbd3d20293633753c44299033f4b10f6d5e3bc2
ntutland <56928897+ntutland@users.noreply.github.com>
2024-06-07 08:49:38 -0400

    New figure with CubCreek2

--- site_selection_plot.R
+++ site_selection_plot.R
@@ -15,10 +15,10 @@ slope_mask <- function(fire_name){
   return(slope_rcl)
 }
 
-perimeter <- vect(here("CedarCreek","CedarCreek_perimeter.shp"))
-drainage <- vect(here("CedarCreek","CedarCreek_drainage_prox_200.shp"))
-sites <- vect(here("CedarCreek","CedarCreek_sample_sites_NEW.shp"))
-severity <- rast(here("CedarCreek", "Severity_Class_510m.tif"))
+perimeter <- vect(here("CubCreek2","CubCreek2_perimeter.shp"))
+drainage <- vect(here("CubCreek2","CubCreek2_drainage_prox_200.shp"))
+sites <- vect(here("CubCreek2","CubCreek2_sample_sites_NEW2.shp"))
+severity <- rast(here("CubCreek2", "Severity_Class_510m.tif"))
 severity <- project(severity, crs(perimeter))
 all_streams <- vect(here("Streams_NorthAmerica","riv_pfaf_7_MERIT_Hydro_v07_Basins_v01_bugfix1.shp"))
 all_streams <- project(all_streams, perimeter)
@@ -27,45 +27,59 @@ streams <- mask(streams, perimeter)
 
 buff <- buffer(perimeter, -500)
 drain_buff_mask <- terra::intersect(drainage, buff)
-drain_buff_inverse <- erase(drainage, buff)
+perimeter_masked <- erase(perimeter, drain_buff_mask)
 
-steep <- slope_mask("CedarCreek")
-steep <- mask(steep, buff)
-# steep_mask <- mask(steep, drain_buff_mask)
-steep_mask <- steep_mask %>% mutate(slope = as.numeric(slope))
-# steep_mask[steep_mask==1] <- 100
+severity_stack <- rast(here("CubCreek2","severity_stack.tif"))
+homogeneity_stack <- rast(here("CubCreek2","homogeneity_stack.tif"))
 
-steep_mask_30m <- project(steep_mask, severity, method = "near")
-severity_in <- mask(severity,steep_mask_30m)
-low_in <- aggregate(as.polygons(severity_in$low),dissolve=T)
-mod_in <- aggregate(as.polygons(severity_in$moderate),dissolve=T)
-high_in <- aggregate(as.polygons(severity_in$high),dissolve=T)
-severity_out <- mask(severity,steep_mask_30m, inverse=T)
-severity_out <- mask(severity_out, buff)
-low_out <- aggregate(as.polygons(severity_out$low),dissolve=T)
-mod_out <- aggregate(as.polygons(severity_out$moderate),dissolve=T)
-high_out <- aggregate(as.polygons(severity_out$high),dissolve=T)
+to_vect <- function(x){
+  x[x==0] <- NA
+  y = as.polygons(x, aggregate=T)
+  return(y)
+} 
+
+high_homo <- to_vect(severity_stack$high*homogeneity_stack$homogeneous)
+high_hetero <- to_vect(severity_stack$high*homogeneity_stack$heterogeneous)
+mod_homo <- to_vect(severity_stack$moderate*homogeneity_stack$homogeneous)
+mod_hetero <- to_vect(severity_stack$moderate*homogeneity_stack$heterogeneous)
+low_homo <- to_vect(severity_stack$low*homogeneity_stack$homogeneous)
+low_hetero <- to_vect(severity_stack$low*homogeneity_stack$heterogeneous)
+high_homo$severity <- "High"
+high_hetero$severity <- "High"
+mod_homo$severity <- "Moderate"
+mod_hetero$severity <- "Moderate"
+low_homo$severity <- "Low"
+low_hetero$severity <- "Low"
+high_homo$heterogeneity <- "Homogeneous"
+high_hetero$heterogeneity <- "Heterogeneous"
+mod_homo$heterogeneity <- "Homogeneous"
+mod_hetero$heterogeneity <- "Heterogeneous"
+low_homo$heterogeneity <- "Homogeneous"
+low_hetero$heterogeneity <- "Heterogeneous"
+
+sev_classes <- rbind(high_homo,high_hetero,mod_homo,mod_hetero,low_homo,low_hetero)
 
 orange <- colorblind_pal()(8)[2]
 blue <- colorblind_pal()(8)[3]
 yellow <- colorblind_pal()(8)[5]
 
+my_colors <- c("purple4",blue,orange)
+
 site_selection <- ggplot() +
   geom_spatvector(data = perimeter, color = "black", linewidth = 0.7, fill = NA) +
-  geom_spatvector(data = buff, color = "black", fill = NA) +
-  geom_spatvector(data = low_in, color = NA, fill=blue) +
-  geom_spatvector(data = mod_in, color = NA, fill=orange) +
-  geom_spatvector(data = high_in, color = NA, fill="purple4") +
-  geom_spatvector(data = low_out, color = NA, fill=blue, alpha = 0.25) +
-  geom_spatvector(data = mod_out, color = NA, fill=orange, alpha = 0.25) +
-  geom_spatvector(data = high_out, color = NA, fill="purple4", alpha = 0.25) +
-  geom_spatvector(data = streams, color = "blue") +
-  geom_spatvector(data = sites, pch = 22, fill = yellow, color = "black", size = 3) +
-  labs(title = "Cedar Creek (WA)") +
+  geom_spatvector(data = sev_classes, aes(fill=severity,alpha=heterogeneity), color = NA) +
+  scale_fill_manual(values = my_colors) +
+  scale_alpha_discrete(range = c(0.7,1)) +
+  geom_spatvector(data = perimeter_masked, color = "black", fill = "white") +
+  geom_spatvector(data = sites, pch = 22, fill = NA, color = "black", size = 3) +
+  labs(title = "Cub Creek 2 (WA)",
+       fill = "Severity\nClass",
+       alpha = "Heterogeneity\nClass") +
   theme_bw() +
-  theme(legend.position = "none",
-        axis.ticks = element_blank(),
-        axis.text = element_blank())
+  theme(axis.ticks = element_blank(),
+        axis.text = element_blank(),
+        legend.position = c(0.15,0.2),
+        legend.background = element_rect(fill=NA))
 site_selection
 
-ggsave(here("Plots","site_selection_NEW.jpg"), site_selection, height = 18, width = 15.5, units = "cm")
+ggsave(here("Plots","site_selection_CubCreek.jpg"), site_selection, height = 18, width = 15.5, units = "cm")
