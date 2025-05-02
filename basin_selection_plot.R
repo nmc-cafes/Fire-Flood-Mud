000d05ce41d9c7305f8516678a7b53afb88b8cc7
ntutland <56928897+ntutland@users.noreply.github.com>
2025-05-02 09:48:28 -0400

    Add basemap

--- basin_selection_plot.R
+++ basin_selection_plot.R
@@ -3,6 +3,7 @@ library(here)
 library(tidyverse)
 library(tidyterra)
 library(ggspatial)
+library(maptiles)
 
 slope_mask <- function(fire_name){
   DEM <- rast(here(fire_name,paste0(fire_name,"_DEM.tif")))
@@ -49,7 +50,15 @@ mtbs_colors <-c("#006400","#7fffd4","#ffff00","#ff0000")
 
 sites <- sites %>% mutate(severe = factor(severe, levels = c(0,1), labels = c("Low Risk","High Risk")))
 
+
+openmap11 <- get_tiles(vect(ext(severity_cropped),crs=crs(severity_cropped)),
+                       provider = "OpenTopoMap", zoom=11, crop = TRUE)
+esrimap11 <- get_tiles(vect(ext(severity_cropped),crs=crs(severity_cropped)), 
+                       provider = "Esri.NatGeoWorldMap", zoom=11, crop = TRUE)
+
+
 basin_selection <- ggplot() +
+  geom_spatraster_rgb(data = esrimap11) +
   geom_spatraster(data = severity_cropped) +
   geom_spatvector(data = perimeter, fill=NA) +
   # scale_fill_manual(values = mtbs_colors, na.value = NA) +
@@ -59,12 +68,14 @@ basin_selection <- ggplot() +
   scale_x_continuous(expand = c(0,0)) +
   scale_y_continuous(expand = c(0,0)) +
   annotation_scale() +
-  theme_bw() + 
+  theme_bw() +
   theme(legend.title = element_blank(),
         legend.position = "inside",
-        legend.position.inside = c(0.82,0.83))
+        legend.position.inside = c(0.82,0.83),
+        axis.text = element_blank(),
+        axis.ticks = element_blank())
 basin_selection
-ggsave("basin_selection_fig.png", basin_selection, path = here("Plots"), width = 13, height = 18, units = "cm")
+ggsave("basin_selection_fig_basemap.png", basin_selection, path = here("Plots"), width = 12, height = 16, units = "cm")
 
 
 zoom <- function(vct, rst, size, x_off, y_off){
