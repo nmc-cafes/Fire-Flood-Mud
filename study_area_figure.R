fac0fe2337a35ee47f20fc0b84baa9a577c2b803
ntutland <56928897+ntutland@users.noreply.github.com>
2025-11-14 15:17:47 -0500

    Create study area fig with soil burn severity

--- study_area_figure.R
+++ study_area_figure.R
@@ -25,6 +25,26 @@ read_severity <- function(path){
   return(rst)
 }
 
+read_SBS <- function(path){
+  rst <- rast(path)
+  rst <- project(rst, "EPSG:5070", method="near")
+  rst[rst==0] <- NA
+  rst[rst==6] <- NA
+  rst[rst==15] <- NA
+  og_name <- names(rst)
+  names(rst) <- "SBS"
+  rst <- rst %>%
+    mutate(SBS = factor(SBS,
+                             labels = c(
+                               "Unburned",
+                               "Low Severity",
+                               "Moderate Severity",
+                               "High Severity"
+                             )))
+  names(rst) <- og_name
+  return(rst)
+}
+
 states <- vect(here("cb_2018_us_state_20m","cb_2018_us_state_20m.shp"))
 conus <- states %>% filter(!NAME %in% c("Alaska","Hawaii","Puerto Rico"))
 conus <- project(conus, "EPSG:5070")
@@ -53,11 +73,16 @@ inset <- ggplot() +
   theme_bw() +
   theme(legend.position="none")
 
-knp_sev <- read_severity(here("KNP","KNP_severity.tif"))
-caldor_sev <- read_severity(here("Caldor","Caldor_severity.tif"))
-dixie_sev <- read_severity(here("Dixie","Dixie_severity.tif"))
-cedar_sev <- read_severity(here("CedarCreek","CedarCreek_severity.tif"))
-cub_sev <- read_severity(here("CubCreek2","CubCreek2_severity.tif"))
+# knp_sev <- read_severity(here("KNP","KNP_severity.tif"))
+# caldor_sev <- read_severity(here("Caldor","Caldor_severity.tif"))
+# dixie_sev <- read_severity(here("Dixie","Dixie_severity.tif"))
+# cedar_sev <- read_severity(here("CedarCreek","CedarCreek_severity.tif"))
+# cub_sev <- read_severity(here("CubCreek2","CubCreek2_severity.tif"))
+knp_sev <- read_SBS(here("KNP","KNP_SBS.tif"))
+caldor_sev <- read_SBS(here("Caldor","Caldor_SBS.tif"))
+dixie_sev <- read_SBS(here("Dixie","Dixie_SBS.tif"))
+cedar_sev <- read_SBS(here("CedarCreek","CedarCreek_SBS.tif"))
+cub_sev <- read_SBS(here("CubCreek2","CubCreek2_SBS.tif"))
 
 mtbs_colors <-c("#006400","#7fffd4","#ffff00","#ff0000","#7fff00")
 
@@ -102,8 +127,13 @@ cub_plot <- ggplot() +
         legend.position = "none")
 
 ggsave("allfires.png", inset, path = here("Plots","study_area_fig"), height = 5.5, width = 4)
-ggsave("knp.png", knp_plot, path = here("Plots","study_area_fig"), height = 4, width = 4)
-ggsave("caldor.png", caldor_plot, path = here("Plots","study_area_fig"), height = 4, width = 4)
-ggsave("dixie.png", dixie_plot, path = here("Plots","study_area_fig"), height = 4, width = 4)
-ggsave("cedar.png", cedar_plot, path = here("Plots","study_area_fig"), height = 4, width = 4)
-ggsave("cub.png", cub_plot, path = here("Plots","study_area_fig"), height = 4, width = 4)
+# ggsave("knp.png", knp_plot, path = here("Plots","study_area_fig"), height = 4, width = 4)
+# ggsave("caldor.png", caldor_plot, path = here("Plots","study_area_fig"), height = 4, width = 4)
+# ggsave("dixie.png", dixie_plot, path = here("Plots","study_area_fig"), height = 4, width = 4)
+# ggsave("cedar.png", cedar_plot, path = here("Plots","study_area_fig"), height = 4, width = 4)
+# ggsave("cub.png", cub_plot, path = here("Plots","study_area_fig"), height = 4, width = 4)
+ggsave("knp_SBS.png", knp_plot, path = here("Plots","study_area_fig"), height = 4, width = 4)
+ggsave("caldor_SBS.png", caldor_plot, path = here("Plots","study_area_fig"), height = 4, width = 4)
+ggsave("dixie_SBS.png", dixie_plot, path = here("Plots","study_area_fig"), height = 4, width = 4)
+ggsave("cedar_SBS.png", cedar_plot, path = here("Plots","study_area_fig"), height = 4, width = 4)
+ggsave("cub_SBS.png", cub_plot, path = here("Plots","study_area_fig"), height = 4, width = 4)
